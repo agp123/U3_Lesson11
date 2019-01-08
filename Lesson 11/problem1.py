896b63f07fa778f856406112049780c8abf26010
agp123 <43734755+agp123@users.noreply.github.com>
2019-01-08 12:21:07 -0500

    Add files via upload

---
+++ Lesson 11/problem1.py
@@ -0,0 +1,11 @@
+print('-' * 60)
+print('Iz iT BrIcK BoT?')
+print()
+
+temperature = input('temperature in Fahrenheit: ')
+temperature = int(temperature)
+
+if temperature >= 32:
+	print('ItZ not bRIck, enjoy ya day')
+else:
+	print('ItZ bRIck, wear ya jacket.')
---
+++ Lesson 11/problem2.py
@@ -0,0 +1,14 @@
+print('-' * 60)
+print('Narrative Bot?')
+print()
+
+Name = input('Student Name: ')
+Grade = input('Grade: ')
+Grade = int(Grade)
+
+if temperature >= 65:
+	print('Narrative: ')
+	print('Name, your final grade in AP Computer Science is grade. You have excelled in all components of the class! I wish you a continued sucess in the next semsester of AP Computer Science!')
+else:
+	print('Narrative:')
+	print('Name, your final grade in AP Computer Sciemnce is 62%')
